7fba10df856fc0e3b1e8b9e9719b8347b59c483a
max <max.chalfin@gmail.com>
2020-12-22 15:07:07 -0500

    pip

--- pip.sh
+++ pip.sh
@@ -3,6 +3,7 @@ pip3 install opencv-contrib-python --user
 pip3 install opencv-python
 pip3 install imutils
 pip3 install sklearn
+pip3 install scikit-image
 pip3 install matplotlib
 # tensorflow is weird and different
 python3 -m pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.12.0-py3-none-any.whl
